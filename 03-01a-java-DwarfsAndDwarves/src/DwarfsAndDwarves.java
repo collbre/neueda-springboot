dcbbe0b61ed0a2eee8d2e0ea57b42ce7ce28bd48
Colleen Breslin <colleenbreslin468@hotmail.com>
2017-11-28 12:02:51 +0000

    03-01a-DwarfsAndDwarves commit

---
+++ 03-01a-java-DwarfsAndDwarves/src/DwarfsAndDwarves.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class DwarfsAndDwarves {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String>dwarfs = new ArrayList<String>();
+		dwarfs.add("Happy");
+		dwarfs.add("Sleepy");
+		dwarfs.add("Dopey");
+		dwarfs.add("Bashful");
+		dwarfs.add("Sneezy");
+		dwarfs.add("Doc");
+		dwarfs.add("Grumpy");
+		
+		ArrayList<String>dwarves = new ArrayList<String>();
+		dwarves.add("Thorin");
+		dwarves.add("Balin");
+		dwarves.add("Dwalin");
+		dwarves.add("Fili");
+		dwarves.add("Kili");
+		dwarves.add("Dori");
+		dwarves.add("Nori");
+		dwarves.add("Ori");
+		dwarves.add("Oin");
+		dwarves.add("Gloin");
+		dwarves.add("Bifur");
+		dwarves.add("Bofur");
+		dwarves.add("Bombur");
+		
+		System.out.println("The Disney Dwarfs are: ");
+		for (String dwarf : dwarfs) {
+			System.out.println(dwarf);
+		}
+		
+		System.out.println("The Hobbit Dwarves are: ");
+		for (String dwarf : dwarves) {
+			System.out.println(dwarf);
+		}
+
+	}
+
+}
