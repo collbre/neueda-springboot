2d772ff79d1ab70f6e84a99f9e0cc6d2ead24da4
Colleen Breslin <colleenbreslin468@hotmail.com>
2017-11-29 13:28:39 +0000

    home page and linked buttons

--- 03-01d-springboot-DwarfsAndDwarves/src/main/java/uk/ac/belfastmet/dwarfs/controller/TolkienConroller.java
+++ 03-01d-springboot-DwarfsAndDwarves/src/main/java/uk/ac/belfastmet/dwarfs/controller/TolkienConroller.java
@@ -1,16 +1,71 @@
 package uk.ac.belfastmet.dwarfs.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import uk.ac.belfastmet.dwarfs.domain.Dwarf;
+
 @Controller
-@RequestMapping("/tolkien/")
+@RequestMapping("/")
 public class TolkienConroller {
 
-	@GetMapping("/")
+	@GetMapping("/tolkien")
 	public String tolkien(Model model) {
+		model.addAttribute("pageTitle", "Tolkien!");
+		model.addAttribute("dwarfs", getDwarfs());
 		return "tolkienPage";
 	}
+	
+	public ArrayList<Dwarf> getDwarfs(){
+		ArrayList<Dwarf> dwarfs = new ArrayList<Dwarf>();
+
+		Dwarf sleepy = new Dwarf("Sleepy", "Disney", "Sleepy.png");
+		dwarfs.add(sleepy);
+		Dwarf happy = new Dwarf("Happy", "Disney", "Happy.png");
+		dwarfs.add(happy);
+		Dwarf dopey = new Dwarf("Dopey", "Disney", "Dopey.png");
+		dwarfs.add(dopey);
+		Dwarf bashful = new Dwarf("Bashful", "Disney", "Bashful.png");
+		dwarfs.add(bashful);
+		Dwarf sneezy = new Dwarf("Sneezy", "Disney", "Sneezy.png");
+		dwarfs.add(sneezy);
+		Dwarf doc = new Dwarf("Doc", "Disney", "Doc.png");
+		dwarfs.add(doc);
+		Dwarf grumpy = new Dwarf("Grumpy", "Disney", "Grumpy.png");
+		dwarfs.add(grumpy);
+		Dwarf thorin = new Dwarf("Thorin", "Tolkien", "Thorin.png");
+		dwarfs.add(thorin);
+		Dwarf balin = new Dwarf("Balin", "Tolkien", "Balin.png");
+		dwarfs.add(balin);
+		Dwarf dwalin = new Dwarf("Dwalin", "Tolkien", "Dwalin.png");
+		dwarfs.add(dwalin);
+		Dwarf fili = new Dwarf("Fili", "Tolkien", "Fili.png");
+		dwarfs.add(fili);
+		Dwarf kili = new Dwarf("Kili", "Tolkien", "Kili.png");
+		dwarfs.add(kili);
+		Dwarf dori = new Dwarf("Dori", "Tolkien", "Dori.png");
+		dwarfs.add(dori);
+		Dwarf nori = new Dwarf("Nori", "Tolkien", "Nori.png");
+		dwarfs.add(nori);
+		Dwarf ori = new Dwarf("Ori", "Tolkien", "Ori.png");
+		dwarfs.add(ori);
+		Dwarf oin = new Dwarf("Oin", "Tolkien", "Oin.png");
+		dwarfs.add(oin);
+		Dwarf gloin = new Dwarf("Gloin", "Tolkien", "Gloin.png");
+		dwarfs.add(gloin);
+		Dwarf bifur = new Dwarf("Bifur", "Tolkien", "Bifur.png");
+		dwarfs.add(bifur);
+		Dwarf bofur = new Dwarf("Bofur", "Tolkien", "Bofur.png");
+		dwarfs.add(bofur);
+		Dwarf bombur = new Dwarf("Bombar", "Tolkien", "Bombur.png");
+		dwarfs.add(bombur);
+		
+		return dwarfs;
+
+	}
+	
 }
